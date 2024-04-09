02713ee141868ff847c6b00509d4e19fbfa16a91
mihirxtc <mihirxtc@gmail.com>
2024-04-09 18:54:07 +0530

    exam programs added

---
+++ gu exam/1.php
@@ -0,0 +1,21 @@
+<html>
+    <body>
+        <form action="1.php" method="post">
+            <input type="number" name="n" placeholder="enter number"> <br>
+            <input type="submit" value="submit">
+        </form>
+        <?php 
+        
+        $n = $_POST['n'];
+        $fact = 1;
+
+        while ($n > 0) {
+            $fact = $fact * $n;
+            $n--;
+        }
+
+        echo "factorial: ".$fact;
+
+        ?>
+    </body>
+</html>
---
+++ gu exam/2.php
@@ -0,0 +1,36 @@
+<!-- update empname to 'ABC' where empnum is 3 -->
+<html>
+    <body>
+        <form action="2.php" method="post">
+            <input type="number" name="eid" placeholder="enter id"> <br>
+            <input type="text" name="ename" placeholder="enter a name"> <br>
+            <input type="submit" value="submit">
+        </form>
+        <?php 
+        
+        if (isset($_POST)) {
+
+            $con = mysqli_connect("localhost", "root", "", "exam");
+            if (!$con) {
+                die("could not connect".mysqli_connect_error());
+            }
+            echo "connection successful <br>";
+
+            $eid = $_POST['eid'];
+            $ename = $_POST['ename'];
+
+            $query = mysqli_query($con, "UPDATE emp SET empname='$ename' WHERE empnum=".$eid);
+            $result = mysqli_affected_rows($con);
+
+            if ($result == 1) {
+                echo "record updated successfully";
+            } else {
+                echo "cannot update record".mysqli_error($con);
+            }
+        }
+
+        mysqli_close($con);
+
+        ?>
+    </body>
+</html>
